73c23d8db3eebfceb6d015867d71202d82e3bc6e
JSalazar00 <jsordlrz@gmail.com>
2023-01-19 15:59:25 -0600

    adding weather description

--- weather_bot.py
+++ weather_bot.py
@@ -38,14 +38,30 @@ class WeatherBot:
         to show the current weather. We will use the user's responses from the getuserinfo function.
         
         """
+        # Greeting
+        msg = "Hi, " + self.name
+        self.sense.show_message(msg, scroll_speed=0.07)
+        
+        # Send request to OpenWeatherMap API
         url = "https://api.openweathermap.org/data/2.5/weather?q=" + self.city + "," + self.state + "," + self.country + "&appid=" + self.key + "&units="+ self.unit
         response = requests.get(url).json()
         
-        temp = response["main"]["temp"]
+        temp = str(int(response["main"]["temp"]))
         weather = response["weather"][0]["main"]
+        dsc = response["weather"][0]["description"]
         
         # To do is to call animation function
         self.animation(weather)
+        
+        # Display temperature
+        if self.tempUnit == "F":
+            msg = "Temp is " + temp + "F"
+        elif self.tempUnit == "C":
+            msg = "Temp is " + temp + "C"
+        self.sense.show_message(msg, scroll_speed=0.06)
+        
+        self.sense.show_message(dsc, scroll_speed=0.06)
+        
         
     def animation(self, weather):
         """
@@ -53,7 +69,7 @@ class WeatherBot:
 
         """
         if weather == "Clear":
-            for x in range(0, 4):
+            for x in range(0, 2):
                 self.sense.set_pixels(clear_skies)
                 time.sleep(0.5)
                 self.sense.set_pixels(clear_skies2)
@@ -63,21 +79,21 @@ class WeatherBot:
                 self.sense.set_pixels(clear_skies4)
                 time.sleep(0.5)
         elif weather == "Clouds":
-            for x in range(0, 4):
+            for x in range(0, 2):
                 self.sense.set_pixels(cloud)
                 time.sleep(0.5)
                 self.sense.set_pixels(cloud2)
                 time.sleep(0.5)
                 self.sense.set_pixels(cloud3)
                 time.sleep(0.5)
-        elif weather = "Rain":
-            for x in range(0, 4):
+        elif weather == "Rain":
+            for x in range(0, 2):
                 self.sense.set_pixels(rain)
                 time.sleep(0.5)
                 self.sense.set_pixels(rain2)
                 time.sleep(0.5)
         elif weather == "Blizzard":
-            for x in range(0,4):
+            for x in range(0, 2):
                 self.sense.set_pixels(blizzard)
                 time.sleep(0.5)
                 self.sense.set_pixels(blizzard2)
